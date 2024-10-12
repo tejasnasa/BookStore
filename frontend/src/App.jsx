87fa0c28c54aefb5e1897fa8ab397857a04e72a8
tejasnasa <tejasnasa2005@gmail.com>
2024-10-12 15:43:22 +0530

    Create home and details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <Route path="/books/create" element={<CreateBook />}></Route>
       <Route path="/books/edit/:id" element={<EditBook />}></Route>
       <Route path="/books/delete/:id" element={<DeleteBook />}></Route>
-      <Route path="/show/:id" element={<ShowBook />}></Route>
+      <Route path="/books/details/:id" element={<ShowBook />}></Route>
     </Routes>
   );
 };
