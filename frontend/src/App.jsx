302eb2dc0fdd0e5f729c5a1ad36ed50b2042a765
tejasnasa <tejasnasa2005@gmail.com>
2024-10-12 15:09:19 +0530

    Create page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
 
 const App = () => {
-  return <div className="bg-red-400 text-white">App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/books/create" element={<CreateBook />}></Route>
+      <Route path="/books/edit/:id" element={<EditBook />}></Route>
+      <Route path="/books/delete/:id" element={<DeleteBook />}></Route>
+      <Route path="/show/:id" element={<ShowBook />}></Route>
+    </Routes>
+  );
 };
 
 export default App;
