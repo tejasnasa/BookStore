09dfcd640d36443280a19ed9c7127c1baddc6d04
tejasnasa <tejasnasa2005@gmail.com>
2024-10-12 12:47:16 +0530

    Create add book functionality

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,37 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome to the bookstore!");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(500).send({
+        message: "Send all 3 values",
+      });
+    }
+    const NewBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(NewBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
---
+++ backend/models/bookModel.js
@@ -0,0 +1,21 @@
+import mongoose, { model } from "mongoose";
+
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Book = mongoose.model("Cat", bookSchema);
