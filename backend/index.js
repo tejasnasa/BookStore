4e221a95c343c60e4637bee92af693335ea0fd00
tejasnasa <tejasnasa2005@gmail.com>
2024-10-12 13:22:26 +0530

    Create CRUD routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import "dotenv/config";
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import { Book } from "./models/bookModel.js";
 
 const app = express();
@@ -28,6 +28,69 @@ app.post("/books", async (req, res) => {
     };
     const book = await Book.create(NewBook);
     return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all 3 values",
+      });
+    }
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book updated successfully!" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndDelete(id, req.body);
+
+    if (!result) {
+      res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book deleted successfully!" });
   } catch (error) {
     console.log(error.message);
     res.status(500).send({ message: error.message });
